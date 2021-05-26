0bc51004a28c5739f17d4320e7bfe008492fe033
yuziem14 <yuri.fnunes@gmail.com>
2021-05-26 16:08:44 -0300

    handle register errors

--- app/src/main/java/com/yuziem14/supertrivia/fragments/RegisterFragment.kt
+++ app/src/main/java/com/yuziem14/supertrivia/fragments/RegisterFragment.kt
@@ -1,20 +1,20 @@
 package com.yuziem14.supertrivia.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.yuziem14.supertrivia.R
 import com.yuziem14.supertrivia.dao.UserDAO
 import com.yuziem14.supertrivia.models.User
-import kotlinx.android.synthetic.main.fragment_auth.view.*
 import kotlinx.android.synthetic.main.fragment_register.*
+import kotlinx.android.synthetic.main.fragment_register.view.*
 
 class RegisterFragment : Fragment() {
+    private lateinit var inflatedView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,11 +22,12 @@ class RegisterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_register, container, false)
-        view.authLink.setOnClickListener { goToAuth() }
-        view.registerButton.setOnClickListener {
-            view.let {
+    ): View {
+        this.inflatedView = inflater.inflate(R.layout.fragment_register, container, false)
+
+        inflatedView.authLink.setOnClickListener { goToAuth() }
+        inflatedView.registerButton.setOnClickListener {
+            inflatedView.let {
                 val name = nameInput.text.toString()
                 val email = emailInput.text.toString()
                 val password = passwordInput.text.toString()
@@ -35,16 +36,46 @@ class RegisterFragment : Fragment() {
                 handleRegister(name, email, password, confirmPassword)
             }
         }
-        return view
+        return inflatedView
     }
 
-    private fun validate(password: String, confirmPassword: String): Boolean {
-        return password.equals(confirmPassword) && password.length > 5
+    private fun validatePassword(password: String, confirmPassword: String): HashMap<String, String> {
+        val errorsMap = HashMap<String, String>()
+        val resources = context?.resources
+        if(password.length < 6) {
+            resources?.let {
+                errorsMap.put("password",
+                    it.getString(R.string.is_too_short, it.getString(R.string.password)),
+                )
+            }
+        }
+        if(!password.equals(confirmPassword)) {
+            resources?.let {
+                errorsMap.put("confirmPassword",
+                    it.getString(R.string.fields_mismatch, it.getString(R.string.confirm_password), it.getString(R.string.password)),
+                )
+            }
+        }
+
+        return errorsMap
     }
 
     private fun handleRegister(name: String, email: String, password: String, confirmPassword: String) {
-        if(!validate(password, confirmPassword)) {
-            Toast.makeText(context, "Confira sua senha e a confirmação", Toast.LENGTH_SHORT).show()
+        val passwordErrors = validatePassword(password, confirmPassword)
+        if(passwordErrors.isNotEmpty()) {
+            if(passwordErrors["password"] != null) {
+                inflatedView.passwordErrorsText.apply {
+                    visibility = View.VISIBLE
+                    inflatedView.passwordErrorsText.text = passwordErrors["password"]
+                }
+            }
+            if(passwordErrors["confirmPassword"] != null) {
+                inflatedView.confirmPasswordErrorsText.apply {
+                    visibility = View.VISIBLE
+                    text = passwordErrors["confirmPassword"]
+                }
+
+            }
             return
         }
 
@@ -52,13 +83,51 @@ class RegisterFragment : Fragment() {
         UserDAO().register(user,
             {
                 response ->
-                Log.d("response", response.toString())
                 this.goToAuth()
             },
             {
                 error ->
-                Toast.makeText(context, "Houve um erro com seu registro! Tente novamente.", Toast.LENGTH_SHORT).show()
+                if(error.data.errors.name.isNotEmpty()) {
+                    inflatedView.nameErrorsText.apply {
+                        text = getErrorString(resources.getString(R.string.name), error.data.errors.name.first()).toString()
+                        visibility = View.VISIBLE
+                    }
+                }
+                if(error.data.errors.email.isNotEmpty()) {
+                    inflatedView.emailErrorsText.apply {
+                        text = getErrorString(resources.getString(R.string.email), error.data.errors.email.first()).toString()
+                        visibility = View.VISIBLE
+                    }
+                }
+                if(error.data.errors.password.isNotEmpty()) {
+                    inflatedView.passwordErrorsText.apply {
+                        text = getErrorString(resources.getString(R.string.password), error.data.errors.password.first()).toString()
+                        visibility = View.VISIBLE
+                    }
+                }
             })
+    }
+
+    private fun getErrorString(fieldName: String, error: String): String? {
+        return when {
+            Regex("can\'t be blank").containsMatchIn(error) -> context?.resources?.getString(
+                R.string.cant_be_blank,
+                fieldName
+            )
+            Regex("has already been taken").containsMatchIn(error) -> context?.resources?.getString(
+                R.string.has_already_been_taken,
+                fieldName
+            )
+            Regex("is too short").containsMatchIn(error) -> context?.resources?.getString(
+                R.string.is_too_short,
+                fieldName
+            )
+            Regex("is invalid").containsMatchIn(error) -> context?.resources?.getString(
+                R.string.is_invalid,
+                fieldName
+            )
+            else -> null
+        }
     }
 
     private fun goToAuth() {
