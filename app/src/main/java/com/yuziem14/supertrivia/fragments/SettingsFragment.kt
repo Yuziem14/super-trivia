9862aa39a2ff972a1e30502fda9a4b909038e9a3
yuziem14 <yuri.fnunes@gmail.com>
2021-05-29 15:19:17 -0300

    implement user profile and logout feature
    Co-authored-by: matheuslk <matheus.augustolk@gmail.com>

--- app/src/main/java/com/yuziem14/supertrivia/fragments/SettingsFragment.kt
+++ app/src/main/java/com/yuziem14/supertrivia/fragments/SettingsFragment.kt
@@ -1,13 +1,20 @@
 package com.yuziem14.supertrivia.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yuziem14.supertrivia.MainActivity
 import com.yuziem14.supertrivia.R
+import com.yuziem14.supertrivia.SessionActivity
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment : Fragment() {
+    private lateinit var inflatedView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,6 +23,35 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        this.inflatedView = view
+
+        this.setupProfile()
+        this.inflatedView.logoutButton.setOnClickListener { logout() }
+        return view
+    }
+
+    private fun setupProfile() {
+        activity?.getSharedPreferences("auth", Context.MODE_PRIVATE)?.apply {
+            inflatedView.usernameText.text = getString("name", "")
+            inflatedView.userEmailText.text = "<${getString("email", "")}>"
+
+        }
+    }
+
+    private fun logout() {
+        activity?.getSharedPreferences("auth", Context.MODE_PRIVATE)?.edit()?.apply {
+            clear()
+            apply()
+        }
+
+        goToLogin()
+    }
+
+    fun goToLogin() {
+        val intent = Intent(activity, SessionActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
     }
 }
